399fdc7e7c6b5dbc2c5d729b3a94bce8ad61f26c
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-06-04 19:27:15 -0500

    add heroku

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const path = require('path');
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+const fs = require('fs');
+const util = require('util');
 
-const PORT = 3001;
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+    
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('./develop/public'));
+
+app.get('/api/notes', function (req, res) {
+    readFileAsync('./develope/db/db.json', 'utf8').then(function(data) {
+        notes = [].concat(JSON.parse(data));
+        res.json(notes);
+    })
+});
+
+app.post('/api/notes', function (req, res,) {
+    const note = req.body;
+    readFileAsync('./develop/db/db.json', 'utf8').then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        writeFileAsync('./develope/db/db.json', JSON.stringify(notes))
+        res.json(note);
+    })
+});
+
+
+app.listen(PORT, function() {
+    console.log('App listing on PORT ' + PORT);
+});
