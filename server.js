1252f19e9c3cd377752245a01a2e0f33e2594d34
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-06-05 19:38:55 -0500

    rewrote my app.get and app.posts

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
+const uuid = require('uuid');
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+
+// const readFileAsync = util.promisify(fs.readFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,44 +14,34 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, './develop/public/index.html'));
-// });
-
-app.get('/api/notes', function (req, res) {
-    readFileAsync('./develope/db/db.json', 'utf8').then(function(data) {
-        notes = [].concat(JSON.parse(data));
-        res.json(notes);
-    })
-});
-
-app.post('/api/notes', function (req, res,) {
-    const note = req.body;
-    readFileAsync('./develop/db/db.json', 'utf8').then(function(data) {
-        const notes = [].concat(JSON.parse(data));
-        note.id = notes.length + 1
-        notes.push(note);
-        return notes
-    }).then(function(notes) {
-        writeFileAsync('./develope/db/db.json', JSON.stringify(notes))
-        res.json(note);
-    })
-});
-
-app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, './develop/public/notes.html'));  
-});
 
 app.get('/', function(req, res)  {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'));
-  });
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-  app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, './develop/public/index.html'));
-  });
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));  
+});
+
+app.get('/api/notes/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+app.post('/api/notes/', function (req, res) {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes);
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, function() {
     console.log('App listing on PORT ' + PORT);
